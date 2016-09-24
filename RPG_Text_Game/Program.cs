1e48fdc9851a30ed8b0ae47ccf5753af246e97fc
cwesnow2000 <cwesnow2000@yahoo.com>
2016-09-23 21:40:22 -0500

    Initial Commit

---
+++ RPG_Text_Game/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Text_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+            string input = "";
+
+            do
+            {
+                Console.WriteLine("Your command: ");
+                input = Console.ReadLine();
+                if (input.ToLower() == "exit" || input.ToLower() == "quit") running = false;
+            }
+            while (running) ;
+        }
+    }
+}
